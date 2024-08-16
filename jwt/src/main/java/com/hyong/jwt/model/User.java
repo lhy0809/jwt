b6d789c43910969e803a3abf0fcee9788e200b38
hyong <hyong@hyong>
2024-08-16 12:02:14 +0900

    [init] init commit - java jwt 의존성 추가 - spring boot 기본 설정 추가 (datasource, jpa)

---
+++ jwt/src/main/java/com/hyong/jwt/model/User.java
@@ -0,0 +1,31 @@
+package com.hyong.jwt.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String username;
+	private String password;
+	private String roles;
+	
+	public List<String> getRoleList() {
+		if(this.roles.length() > 0) {
+			return Arrays.asList(roles.split(","));
+		}
+		return new ArrayList<>();
+	}
+}
