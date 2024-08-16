17539ec106059188668fa63dc6ca87f76d0c4b63
hyong <hyong@hyong>
2024-08-16 14:21:36 +0900

    [test] filter 및 token 생성 테스트

--- jwt/src/main/java/com/hyong/jwt/controller/RestApiController.java
+++ jwt/src/main/java/com/hyong/jwt/controller/RestApiController.java
@@ -1,9 +1,7 @@
 package com.hyong.jwt.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +10,10 @@ public class RestApiController {
 	@GetMapping("home")
 	public String home() {
 		return "<h1>home</h1>";
+	}
+	
+	@PostMapping("token")
+	public String token() {
+		return "<h1>token</h1>";
 	}
 }
--- jwt/src/main/java/com/hyong/jwt/filter/MyFilter1.java
+++ jwt/src/main/java/com/hyong/jwt/filter/MyFilter1.java
@@ -7,14 +7,40 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
 public class MyFilter1 implements Filter {
 	
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		System.out.println("필터1");
-		chain.doFilter(request, response);
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		
+		String headerAuth = (String) req.getHeader("Authorization");
+		
+		// 토근 생성
+		if("cos".equals(headerAuth)) {
+			System.out.println("토큰 : " + headerAuth);
+			chain.doFilter(req, res);
+		}else {
+			res.getWriter().println("인증안됨");
+		}
+		
+		/**
+		 * 요청 시 마다 filter 작동 됨 : 필터2(security filter before) > 필터3(security filter after) > 토큰 : cos(doFilter)
+		 * javascript post 요청 방법
+		 * 
+		 * 1. var xhr = new XMLHttpRequest;
+		 * 2. xhr.open('POST', 'http://localhost:8080/token', true);
+		 * 3. xhr.setRequestHeader('Content-Type', 'application/json');
+		 * 4. xhr.setRequestHeader('Authorization', 'cos');
+		 * 5. var data = JSON.stringify({ key1: 'value1', key2: 'value2' }); //json 데이터 세팅
+		 * 6. xhr.send(data);
+		 * 
+		 */
 	}
 
 }
